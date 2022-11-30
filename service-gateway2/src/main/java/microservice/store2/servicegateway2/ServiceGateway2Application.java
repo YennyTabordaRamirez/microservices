66af34d73eede26bf8c2a81c934456778dc3a9e9
Yenny_TR <tarayenny@gmail.com>
2022-11-30 18:58:59 -0500

    adding gateway class and config

---
+++ service-gateway2/src/main/java/microservice/store2/servicegateway2/ServiceGateway2Application.java
@@ -0,0 +1,13 @@
+package microservice.store2.servicegateway2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGateway2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceGateway2Application.class, args);
+	}
+
+}
---
+++ service-gateway2/src/test/java/microservice/store2/servicegateway2/ServiceGateway2ApplicationTests.java
@@ -0,0 +1,13 @@
+package microservice.store2.servicegateway2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGateway2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
