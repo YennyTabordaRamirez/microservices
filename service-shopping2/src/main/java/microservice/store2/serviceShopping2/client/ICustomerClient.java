aa1d7a3b1bf1ae0429dfff8815b17f7132b7c7d3
Yenny_TR <tarayenny@gmail.com>
2022-11-30 18:36:58 -0500

    adding feign

--- service-shopping2/src/main/java/microservice/store2/serviceShopping2/client/ICustomerClient.java
+++ service-shopping2/src/main/java/microservice/store2/serviceShopping2/client/ICustomerClient.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "customer-service")
-@RequestMapping(value = "/customers")
+@FeignClient(name = "customer-service", path = "/customers")
+//@RequestMapping(value = "/customers")
 public interface ICustomerClient {
 
     @GetMapping(value= "/{id}")
