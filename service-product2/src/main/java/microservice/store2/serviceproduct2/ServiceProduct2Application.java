b1b62d5f6aa06b7e02f7ddb5d8506df7e450b753
Yenny_TR <tarayenny@gmail.com>
2022-11-30 14:02:39 -0500

    Is modified the port 8089 to 8761

--- service-product2/src/main/java/microservice/store2/serviceproduct2/ServiceProduct2Application.java
+++ service-product2/src/main/java/microservice/store2/serviceproduct2/ServiceProduct2Application.java
@@ -5,13 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@Configuration
-//@EnableAutoConfiguration(
-//		exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableEurekaClient
 public class ServiceProduct2Application {
 
 	public static void main(String[] args) {
